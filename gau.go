bf02db3244f16e3e99722ad90b227582474f85df
Jinone <44525519+Jinone@users.noreply.github.com>
2020-04-12 18:05:13 +0800

    Update gau.go

--- gau.go
+++ gau.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"bufio"
+	"crypto/md5"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
+	"regexp"
+	"strings"
 	"time"
 )
 
@@ -31,15 +36,65 @@ type CommonCrawlInfo []struct {
 
 var IncludeSubs bool
 var MaxRetries int
-
+var HttpStatus bool
 var client = &http.Client{
 	Timeout: time.Second * 15,
+}
+var ResultData []Data
+
+type Data struct {
+	Host     string
+	Path     string
+	QueryKey []string
+	Hash     string
+}
+
+type Url struct {
+	Url string
+}
+
+func HandleUri(uri string) Data {
+	u, err := url.Parse(uri)
+	data := Data{}
+	if err != nil {
+		return data
+	}
+	data.Host = u.Host
+	reg, _ := regexp.Compile(`(\d+)`)
+	data.Path = reg.ReplaceAllString(u.Path, "1")
+	hashString := data.Host + data.Path
+	for _, param := range strings.Split(u.RawQuery, "&") {
+		key := strings.Split(param, "=")[0]
+		data.QueryKey = append(data.QueryKey, key)
+		hashString += key
+	}
+	data.Hash = Md5(hashString)
+	return data
+}
+
+func Md5(content string) string {
+	h := md5.New()
+	h.Write([]byte(content))
+	cipherStr := h.Sum(nil)
+	return hex.EncodeToString(cipherStr)
+}
+
+func DeDuplication(uri string) bool {
+	data := HandleUri(uri)
+	for _, item := range ResultData {
+		if item.Hash == data.Hash {
+			return true
+		}
+	}
+	ResultData = append(ResultData, data)
+	return false
 }
 
 func main() {
 	var domains []string
 	flag.BoolVar(&IncludeSubs, "subs", false, "include subdomains of target domain")
 	flag.IntVar(&MaxRetries, "retries", 5, "amount of retries for http client")
+	flag.BoolVar(&HttpStatus, "httpcode", false, "get httpcode")
 	flag.Parse()
 	if flag.NArg() > 0 {
 		domains = []string{flag.Arg(0)}
@@ -50,7 +105,7 @@ func main() {
 		}
 	}
 	for _, domain := range domains {
-		Run(domain)
+			Run(domain)
 	}
 }
 
@@ -65,7 +120,15 @@ func Run(domain string) {
 			continue
 		}
 		for _, f := range found {
-			fmt.Println(f)
+			if !HttpStatus {
+				if !DeDuplication(f){
+					fmt.Println(f)
+				}
+			} else {
+				if !DeDuplication(f) {
+					fmt.Println(HttpCode(f))
+				}
+			}
 		}
 	}
 }
@@ -222,4 +285,24 @@ func getCurrentCC() (string, error) {
 		return "", errors.New("unexpected response from commoncrawl.")
 	}
 	return wrapper[0].API, nil
+}
+
+func HttpCode(url string) (x string){
+	baseHost := url
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+		Timeout: 3 * time.Second,
+	}
+	res, err := client.Get(baseHost)
+	if err != nil {
+		return ""
+	}
+	if res.StatusCode != 301 && res.StatusCode != 302 && res.StatusCode != 303 && res.StatusCode != 304 {
+		k :=fmt.Sprintf("%s %d len:%d",url,res.StatusCode,res.ContentLength)
+		return k
+	}
+	g := fmt.Sprintf("%s %d  %s len:%d",url,res.StatusCode,res.Header.Get("Location"),res.ContentLength)
+	return g
 }
